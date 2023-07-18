1b21b10efe90603a680fa50082bee00df76ff56a
yavuztur <108802497+yavuztur@users.noreply.github.com>
2023-07-18 10:39:57 +0300

    Add files via upload

---
+++ online-booking/app/layout.js
@@ -0,0 +1,19 @@
+import { Inter  } from '@next/font/google'
+import '../styles/global.css';
+
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+
+const interFontFamily=Inter({subsets:['latin']});
+export default function Rootlayout({ children}){
+  return (
+    <html lang='tr' className={interFontFamily.className}>
+      <body className='container'>
+        <Header />
+        <main>{ children}</main>
+        <Footer />
+
+        </body>
+    </html>
+  );
+}
---
+++ online-booking/components/footer/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Footer(){
+    return <div>Footer</div>;
+}
+export default Footer;
